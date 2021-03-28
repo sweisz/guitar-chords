439b83484bb8fae3c854725b80dbfab8eaade402
Steffen Weisz <dev@steffenweisz.de>
2021-03-28 17:46:53 +0200

    Add a Startpage (#12)
    * ✨ Create a Start Page in next.js
    - Header
    - SubHeader
    - Logo with Link
    - Teasertext
    
    * 💄 Styled the Start Page
    
    * 💄 Edit CSS for use in Start Page
    - Logo.modules.css
    - Header.modules.css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,36 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Header from "../components/Headers/Header";
+import SubHeader from "../components/Headers/SubHeader";
+import Logo from "../components/Logo/Logo";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Guitar Chords</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <div className={styles.fullHeader}>
+          <Header label="Guitar" size="big" />
+          <SubHeader label="Chords" size="small" />
+        </div>
+        <div className={styles.logo}>
+          <Link href="/skill">
+            <a>
+              <Logo size="small" />
+            </a>
+          </Link>
+        </div>
+
+        <p className={styles.introText}>
+          Learn to play guitar
+          <br /> with ease.
+        </p>
+      </main>
     </div>
   );
 }
