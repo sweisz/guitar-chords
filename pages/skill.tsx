722708a6960855fdd37e820095cb64dd966f2e28
Steffen Weisz <dev@steffenweisz.de>
2021-03-29 11:51:18 +0200

    Add a Skill-Page (#13)
    * ✨ Create a Start Page in next.js
    - Header
    - SubHeader
    - Logo with Link
    - Teasertext
    
    * 💄 Styled the Start Page
    
    * 💄 Edit CSS for use in Start Page
    - Logo.modules.css
    - Header.modules.css
    
    * ⚰️ Remove comment in css
    
    * ✨ Add a skill page
    - Skill page Button, Logo, Headers components
    
    * ♻️ Change title of skill page

---
+++ pages/skill.tsx
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Header from "../components/Headers/Header";
+import SubHeader from "../components/Headers/SubHeader";
+import Logo from "../components/Logo/Logo";
+import Button from "../components/button/Button";
+import Link from "next/link";
+
+export default function Skill() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Guitar Chords – Skills</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.fullHeader}>
+          <Header label="Skill" size="big" />
+          <SubHeader label="Choose your level" size="small" />
+        </div>
+        <div className={styles.logoSmall}>
+          <Link href="/skill">
+            <a>
+              <Logo size="small" />
+            </a>
+          </Link>
+        </div>
+        <div className={styles.skillButtonsWrapper}>
+          <Button label="Beginner" />
+          <Button label="Advanced" />
+        </div>
+      </main>
+    </div>
+  );
+}
