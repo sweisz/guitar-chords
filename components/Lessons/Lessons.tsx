3014cd5f483fa66d73791ec3db9894857cbe6618
sweisz <dev@steffenweisz.de>
2021-04-05 18:28:53 +0200

    ✨ Add Lessons page with - header - list items - button

---
+++ components/Lessons/Lessons.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Lessons.module.css";
+import ListItems from "../ListItems/ListItem";
+import LessonsButton from "../LessonsButton/LessonsButton";
+import LessonsHead from "../LessonsHead/LessonsHead";
+
+function Lessons() {
+  return (
+    <div className={styles.background}>
+      <LessonsHead label={"Beginner"} link={"/beginner"} />
+      <ListItems title={"Stairway To Heaven - Led Zeppelin"} checkBox={false} />
+      <LessonsButton label={"Beginner"} href={"/beginner"} />
+    </div>
+  );
+}
+
+export default Lessons;
---
+++ pages/lessons.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Lessons from "../components/Lessons/Lessons";
+
+export default function LessonsPage() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Guitar Chords – Lessons</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <Lessons />
+      </main>
+    </div>
+  );
+}
