93357b56c642fd47a3607e5f368dd610f7168fc5
sweisz <dev@steffenweisz.de>
2021-04-06 12:33:43 +0200

    Change routes to beginner an advanced

--- pages/skill.tsx
+++ pages/skill.tsx
@@ -27,8 +27,8 @@ export default function Skill() {
           </Link>
         </div>
         <div className={styles.skillButtonsWrapper}>
-          <Button label="Beginner" route="beginner" />
-          <Button label="Advanced" route="advanced" />
+          <Button label="Beginner" route="/lessons/beginner" />
+          <Button label="Advanced" route="/lessons/advanced" />
         </div>
       </main>
     </div>
