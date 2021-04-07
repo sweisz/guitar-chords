898b5e5179ffd669d86fbfa1f0e5f498d36d2824
Steffen Weisz <dev@steffenweisz.de>
2021-04-07 10:46:27 +0200

    Add a Lessons page (#22)
    * ✨ Add Lessons page with
    - header
    - list items
    - button
    
    * 💄 Justify Home.module.css
    
    * 🐛 Fix a deployment error
    - checkbox input has now a unique class
    
    * Change page components
    
    * 🚧 remove SearchInput component property
    
    * 💄 Fixed styles and unnessesary code.
    
    * Rename File and fix imports
    
    * delete unnecessary code
    
    * change style an divs

--- components/Lessons/Lessons.tsx
+++ components/Lessons/Lessons.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import styles from "./Lessons.module.css";
 import ListItems from "../ListItems/ListItem";
-import LessonsButton from "../LessonsButton/LessonsButton";
 import LessonsHead from "../LessonsHead/LessonsHead";
+import ButtonGroup from "../ButtonGroup/ButtonGroup";
+import SearchInput from "../SearchInput/SearchInput";
 
 function Lessons() {
   return (
-    <div className={styles.background}>
+    <div className={styles.container}>
       <LessonsHead label={"Beginner"} link={"/beginner"} />
-      <ListItems title={"Stairway To Heaven - Led Zeppelin"} checkBox={false} />
-      <LessonsButton label={"Beginner"} href={"/lessons/beginner"} />
+
+      <SearchInput />
+      <div>
+        <ListItems
+          title={"Stairway To Heaven - Led Zeppelin"}
+          checkBox={false}
+        />
+      </div>
+      <ButtonGroup />
     </div>
   );
 }
--- pages/lessons.tsx
+++ pages/lessons.tsx
@@ -1,18 +1,17 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Lessons.module.css";
 import Lessons from "../components/Lessons/Lessons";
 
 export default function LessonsPage() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Guitar Chords – Lessons</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
         <Lessons />
       </main>
-    </div>
+    </>
   );
 }
