d4c4d9436bc7b574ab2cf7c247d791df120e9cec
sweisz <dev@steffenweisz.de>
2021-04-08 11:11:50 +0200

    ➕ Add aplphatab.js in Next `Head`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import React from "react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script src="https://cdn.jsdelivr.net/npm/@coderline/alphatab@latest/dist/alphaTab.js" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
