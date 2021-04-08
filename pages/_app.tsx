29f831ac9a518e6200d440d5d0b0560dfa8410f9
sweisz <dev@steffenweisz.de>
2021-04-08 11:32:43 +0200

    ➕ Add fontawesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import React from "react";
 import "../styles/globals.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <script src="https://cdn.jsdelivr.net/npm/@coderline/alphatab@latest/dist/alphaTab.js" />
       </Head>
       <Component {...pageProps} />
